0ff385b73ee4bb90b6744f63c893d0bd9ee99ee2
tianlitao <tianlitao3399@gmail.com>
2022-01-10 13:10:56 +0800

    rename reverse_record

--- app/controllers/api/v1/das_accounts_controller.rb
+++ app/controllers/api/v1/das_accounts_controller.rb
@@ -9,9 +9,9 @@ class Api::V1::DasAccountsController < ActionController::API
                   owner_chain_type_num: Das::AccountInfo.owner_chain_type_num,
                   account_chain_num: Das::AccountInfo.account_chain_num,
                   owner_order: Das::AccountInfo.left_outer_joins(:reverse_info)
-                                   .select("count(*) total,min(t_reverse_info.account) as reserve_record,owner")
+                                   .select("count(*) total,min(t_reverse_info.account) as reverse_record,owner")
                                    .group(:owner).order('total desc').limit(10)
-                                   .as_json(:except => :id).each{|i| i['reserve_record'] = i['reserve_record'].to_s},
+                                   .as_json(:except => :id).each{|i| i['reverse_record'] = i['reverse_record'].to_s},
                   update_time: Time.at(Das::AccountInfo.last.registered_at).strftime('%Y-%m-%d %H:%M:%S')
                   }, status: :ok
   end
